99f5c45a329d78935d9dd436a8e020552bde32d4
John-David Dalton <john.david.dalton@gmail.com>
2016-05-07 21:09:13 -0700

    Use `OccuranceOrderPlugin`.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
 'use strict';
 
 var _ = require('./lodash'),
-  webpack = require('webpack'),
-  env = _.last(process.argv);
+    webpack = require('webpack'),
+    env = _.last(process.argv);
 
 var config = {
   'output': {
     'library': '_',
     'libraryTarget': 'umd'
-  },
-  'plugins': []
+  }
 };
 
 if (env == 'production') {
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+  config.plugins = [
+    new webpack.optimize.OccurenceOrderPlugin,
+    new webpack.optimize.UglifyJsPlugin
+  ];
 }
 
 module.exports = config;
